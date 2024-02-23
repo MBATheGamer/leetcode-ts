4f3ab08bbcf4d4eef694991fef61ebeb7fafa263
MBATheGamer <mbathegamer@gmail.com>
2024-02-23 18:56:49 +0100

    20. Valid Parentheses (Solution)

--- 20-valid-parentheses/index.ts
+++ 20-valid-parentheses/index.ts
@@ -1,2 +1,27 @@
 export function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  const map = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  } as const;
+
+  let i = 0;
+
+  while (i < s.length) {
+    if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+      if (stack[stack.length - 1] == map[s[i]]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(s[i]);
+    }
+
+    i++;
+  }
+
+  return stack.length === 0;
 };
